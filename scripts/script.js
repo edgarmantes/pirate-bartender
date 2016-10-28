8af3d817ab2643cb6080a8f08f7453cb98e3a27a
Edgar Mantes <edgar.mantes11@gmail.com>
2016-10-28 14:51:51 -0500

    All functions are working as spagetti code

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 var answers = [];
+var qTracker = 0;
+var mix = [];
 
-
-
-// questions constructor
+/********************************
+questions constructor
+********************************/ 
 
 var Questions = function(flavor, question){
 	this.question = question;
@@ -21,57 +23,97 @@ var questions = [
 ]
 
 
+/*******************************
+Ingredient contructor function
+*******************************/
 
-// Ingredient contructor function
-
-var Ingredient = function(flavor, ingredient){
+var Ingredient = function(ingredient){
 	this.ingredient = ingredient;
-	this.flavor = flavor;
 }
 
 
 // pantry bank
 
-var pantry = [
+var pantry = {
 	
-	new Ingredient("strong", "Glug of rum, slug of whisky, splash of gin"), 
-	new Ingredient("salty", "Olive on a stick, salt-dusted rim, rasher of bacon"), 
-	new Ingredient("fruity", "Slice of orange, dash of cassis, cherry on top"), 
-	new Ingredient("sweet", "Sugar cube, spoonful of honey, splash of cola")
+	strong : new Ingredient(["Glug of rum", "slug of whisky", "splash of gin"]),
+	salty : new Ingredient(["Olive on a stick", "salt-dusted rim", "rasher of bacon"]),
+	fruity : new Ingredient(["Slice of orange", "dash of cassis", "cherry on top"]), 
+	sweet: new Ingredient(["Sugar cube", "spoonful of honey", "splash of cola"]),
 
-]
+}
 
+// Nay response will alert one of these responses
+var reply = [
+	"Matey, yer missin' out!",
+	"That's the first!",
+	"Shiver me timbers! really?... good thing ye drinkin' this!", 
+	"Ar ye certain? This will put some hair on ye chest!",
+	"The more for me belly!"
+];
 
 var addToList = function(e){
+	var currentQ = $(".js-questions").text();
+	// Searching for the matched question object and pushing to answers array
+	for (var i = 0; i < questions.length; i++){
+		if (questions[i].question === currentQ){
+			answers.push(questions[i]);
+		} 
+	}
+}
+
+var goThroughQuestions = function(e){
 	e.preventDefault();
-	var responseYea = $(this).text();
+	if (qTracker < questions.length) {
+		addToList();
+		nextQuestion();
+	} else {
+		createDrink();
+	}
+}
+
+var createDrink = function(){
+	if ( answers.length === 0){
+	  	$('.js-mix').append("Leave this here place before me take ye to Davey Jones locker! ");	
+	} else {
+
+		answers.forEach(function(object){
+			var flav = object.flavor;
+			var ingred = pantry[flav].ingredient[Math.floor(Math.random() * 3)];
+			mix.push(ingred);
+		})
+		renderToHTML()
+	}
+}
+
+
+
+var renderToHTML = function(){
+	mix.forEach(function(ing){
+		$('.js-mix').append(ing);
+
+	})
 }
 
 var alertMessage = function(e){
 	e.preventDefault();
-	var responseNay = $(this).text();
-
-	var reply = [
-		"Matey, yer missin' out!",
-		"That's the first!",
-		"Shiver me timbers! really?... good thing ye drinkin' this!", 
-		"Ar ye certain? This will put some hair on ye chest!",
-		"The more for me belly!"
-	];
-
 	var replyBack =  reply[Math.floor(Math.random() * 5)];
 	alert(replyBack)
+	if (qTracker < questions.length){
+		nextQuestion();
+	} else {
+		createDrink();
+	}
+}
+
+var nextQuestion = function(){
+	$('.js-questions').empty().append(questions[qTracker].question)
+	qTracker++;
 }
 
 $(document).ready(function(){
-	// User response listeners
-	$(".yes").on('click', addToList);
+	nextQuestion();
+	$(".yes").on('click', goThroughQuestions);
 	$(".no").on('click', alertMessage);
-
-
-	// First questions loads
-	$(".js-questions").html(questions[0].question);
 })
 
-
-//Math.floor(Math.random() * 3)
