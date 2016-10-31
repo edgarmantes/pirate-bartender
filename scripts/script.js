8fe634348a32f3c602e72b4b0d3e4be1c44ebc39
Edgar Mantes <edgar.mantes11@gmail.com>
2016-10-31 18:56:41 -0500

    initiated a package.json and refactored script.js for OOP

--- scripts/script.js
+++ scripts/script.js
@@ -9,9 +9,9 @@
 			return text
 		},
 		init : function(){
-			this.nextQuestion();
+			questions.nextQuestion();
 			$(".yes").on('click', this.goThroughQuestions.bind(this));
-			$(".no").on('click', this.alertMessage.bind(this));
+			$(".no").on('click', Message.alert);
 		},
 		reply : [
 			"Matey, yer missin' out!",
@@ -20,24 +20,24 @@
 			"Ar ye certain? This will put some hair on ye chest!",
 			"The more for me belly!"
 		],
-		addToList : function(){
-			//var currentQ = $(".js-questions").text();
-			// Searching for the matched question object and pushing to answers array
-			for (var i = 0; i < questions.length; i++){
-				if (questions[i].question === this.currentQ()){
-					bartender.answers.push(questions[i]);
-				} 
-			}
-		},
+
 		goThroughQuestions : function(e){
 			e.preventDefault();
-			if (this.qTracker < questions.length) {
-				this.addToList();
-				this.nextQuestion();
-			} else {
-				this.createDrink();
-			}
+			questions.addToList();
+			this.qTracker++;
+			NextStep();
 		},
+
+		// renders created list of ingredients to DOM
+		renderToHTML : function(){
+			this.mix.forEach(function(ing){
+				$('.js-mix').append(ing);
+
+			})
+		}, 
+
+
+		// 
 		createDrink : function(){
 			if ( this.answers.length === 0){
 			  	$('.js-mix').append("Leave this here place before me take ye to Davey Jones locker! ");	
@@ -46,78 +46,87 @@
 				this.answers.forEach(function(object){
 					
 					var flav = object.flavor;
-					var ingred = pantry[flav].ingredient[Math.floor(Math.random() * 3)];
+					var ingred = Pantry.items[flav].ingredient[Math.floor(Math.random() * 3)];
 					bartender.mix.push(ingred);
 				})
-				console.log(bartender.mix)
 				this.renderToHTML()
 			}
 		},
-		renderToHTML : function(){
-			this.mix.forEach(function(ing){
-				$('.js-mix').append(ing);
-
-			})
-		},
-		alertMessage : function(e){
-			e.preventDefault();
-			var replyBack =  this.reply[Math.floor(Math.random() * 5)];
-			alert(replyBack)
-			console.log(this.qTracker)
-			if (this.qTracker < questions.length){
-				this.nextQuestion();
-			} else {
-				bartender.createDrink();
-			}
-		},
-		nextQuestion : function(){
-			$('.js-questions').empty().append(questions[this.qTracker].question)
-			this.qTracker++;
-		},
 		
-	}
+	};
 
+	/***********************************************
+	Alerts a message if you say 'No'
+	***********************************************/
+	var Message = {
+		replyBack : bartender.reply[Math.floor(Math.random() * 5)],
+		alert : function(e){
+			e.preventDefault();
+			alert(Message.replyBack);
+			bartender.qTracker++;
+			NextStep();
+		}
+	};
+
+
+	/*******************************************************
+	Goes to the next question or makes your a drink after all questions have been asked
+	*******************************************************/
+	var NextStep = function(){
+		if (bartender.qTracker < questions.allQuestions.length){
+				questions.nextQuestion();
+		} else {
+			bartender.createDrink();
+		}
+	};
+
+	// pantry with list of ingredience with ad
+	var Pantry = {
+		items : {
+			strong : new Ingredient(["Glug of rum", "slug of whisky", "splash of gin"]),
+			salty : new Ingredient(["Olive on a stick", "salt-dusted rim", "rasher of bacon"]),
+			fruity : new Ingredient(["Slice of orange", "dash of cassis", "cherry on top"]), 
+			sweet: new Ingredient(["Sugar cube", "spoonful of honey", "splash of cola"]),
+		},
+		 
+	};
+
+	/******************************
+	Ingredient contructor function
+	******************************/
+
+
+	function Ingredient(ingredient){
+		this.ingredient = ingredient;
+	}
 
 
 	/********************************
 	questions constructor
 	********************************/ 
 
-	var Questions = function(flavor, question){
+	function Question(flavor, question){
 		this.question = question;
 		this.flavor = flavor;
 	}
 
-	var questions = [
+	var	questions = {
+		// Pushes the instance of the question object into the answers array
+		addToList : function(){
+			bartender.answers.push(questions.allQuestions[bartender.qTracker]);
+		},
+		//empties HTML tag and appends next question from the questions array
+		nextQuestion : function(){
+			$('.js-questions').empty().append(this.allQuestions[bartender.qTracker].question)
+		},
 
-		new Questions("strong", "Do ye like yer drinks strong?"), 
-		new Questions("salty", "Do ye like it with a salty tang?"), 
-		new Questions("fruity", "Are ye one for a fruity finish?"), 
-		new Questions("sweet", "Would ye like a bit of sweetness with yer poison?")
-	]
+		allQuestions : 	[
 
-
-
-
-
-	/******************************
-	Ingredient contructor function
-	******************************/
-
-	var Ingredient = function(ingredient){
-		this.ingredient = ingredient;
-	}
-
-
-	// pantry bank
-
-	var pantry = {
-		
-		strong : new Ingredient(["Glug of rum", "slug of whisky", "splash of gin"]),
-		salty : new Ingredient(["Olive on a stick", "salt-dusted rim", "rasher of bacon"]),
-		fruity : new Ingredient(["Slice of orange", "dash of cassis", "cherry on top"]), 
-		sweet: new Ingredient(["Sugar cube", "spoonful of honey", "splash of cola"]),
-
+			new Question("strong", "Do ye like yer drinks strong?"), 
+			new Question("salty", "Do ye like it with a salty tang?"), 
+			new Question("fruity", "Are ye one for a fruity finish?"), 
+			new Question("sweet", "Would ye like a bit of sweetness with yer poison?")
+		]
 	}
 
 	bartender.init();
